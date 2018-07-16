51d175f3ed23cecf4838b4f06d3efa75c217e486
Richard Kim <rk.sungjin@gmail.com>
2018-07-16 14:32:39 -0700

    react app

---
+++ src/App.js
@@ -0,0 +1,7 @@
+//imports dependencies and files
+import React, { Component } from "react";
+import Jumbotron from "./components/Jumbotron";
+import FriendCard from "./components/FriendCard";
+import fish from "./fish.json";
+import "./App.css";
+// import Footer from "./components/Footer";
